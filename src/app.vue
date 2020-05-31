cd9feab5f0570c2a7941bee18e0dfa63d77bfa84
qiujiahong <qiujiahongde@163.com>
2020-05-31 15:49:27 +0800

    commit pages

--- src/app.vue
+++ src/app.vue
@@ -2,35 +2,39 @@
     <div id="app">
         <div id="cover"></div>
         <Header></Header>
+        <Todo></Todo>
         <Footer></Footer>
     </div>
 </template>
 
 <script>
-import Header from './todo/header.vue'
-import Footer from './todo/footer.jsx'
+    import Header from './todo/header.vue'
+    import Footer from './todo/footer.jsx'
+    import Todo from './todo/todo.vue'
 
-export default {
-    components:{
-      Header,
-        Footer
-    },
-    data(){
-        return {
-            text: 'abc'
+    export default {
+        components: {
+            Header,
+            Footer,
+            Todo
+        },
+        data() {
+            return {
+                text: 'abc'
+            }
         }
     }
-}
 </script>
 <style lang="stylus" scoped>
-    #app{
+    #app {
         position absolute
         left 0
         right 0
         top 0
         bottom 0
     }
-    #cover{
+
+    #cover {
         position absolute
         left 0
         top 0
