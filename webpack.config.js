305c9ce1e94b0f5ffd941510146e43ee0abeabbe
qiujiahong <qiujiahongde@163.com>
2020-05-31 09:33:30 +0800

    [add] add webpack-dev-server support

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,28 @@
 const   path = require("path")
 const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const HTMLPlugin = require('html-webpack-plugin')
+const webpack = require('webpack')
 
-module.exports ={
+const isDev = process.env.NODE_ENV === 'development'
+
+const config ={
+    target: "web",
     entry:  path.join(__dirname,'src/index.js'),
     output: {
         filename: 'bundle.js',
         path: path.join(__dirname,'dist')
     },
     plugins: [
-        new VueLoaderPlugin()
+        new VueLoaderPlugin(),
+        //vue webpack会根据不同的环境区分打包
+        new webpack.DefinePlugin({
+            'process.env':{
+                NODE_ENV: isDev? '"development"' :'"production"'
+            }
+        }),
+        new HTMLPlugin()
     ],
-    mode: "development",
+    mode: process.env.NODE_ENV,
     module: {
         rules: [
             {
@@ -48,3 +60,31 @@ module.exports ={
         ]
     }
 }
+
+
+if (isDev){
+    config.devtool = '#cheap-module-eval-source-map'
+    config.devServer = {
+        port: 8000,
+        host: '0.0.0.0',
+        //有错误显示到网页上
+        overlay: {
+            errors: true
+        },
+        //没有的路由默认跳转到一个页面去
+        // historyFallback:{
+        //
+        // },
+        //启动webpack-dev-server的时候自动帮助打开浏览器
+        open: true,
+        //更改内容之后热加载,HotModuleReplacementPlugin, NoEmitOnErrorsPlugin
+        // hot: true,
+    }
+    // config.plugins.push(
+    //     new webpack.HotModuleReplacementPlugin()
+    //     new webpack.NoEmitOnErrorsPlugin()
+    // )
+}
+
+
+module.exports = config
